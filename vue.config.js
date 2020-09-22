994fb40bbefbfe1cdcbeb2b4eba028afa857669d
崮生 <admin@shenzilong.cn>
2020-09-22 15:18:41 +0800

    ✨feat(更新icon): icon 不能太大也不能太小，256
    还有electron 下载是真的艰难啊

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,24 @@ module.exports = {
   configureWebpack: {
     plugins: [],
   },
+  pluginOptions: {
+    electronBuilder: {
+      builderOptions: {
+        appId: "shenzilong.cn",
+        nsis: {
+          oneClick: false,
+          allowToChangeInstallationDirectory: true,
+        },
+        win: {
+          icon: "./public/favicon.ico",
+        },
+        mac: {
+          icon: "./public/favicon.ico",
+        },
+        linux: {
+          icon: "./public/favicon.ico",
+        },
+      },
+    },
+  },
 };
